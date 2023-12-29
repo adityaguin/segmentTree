2e4d250db59744e766b0cbbaf66482b17aa90b74
Aditya Guin <55221100+adityaguin@users.noreply.github.com>
2023-12-28 20:30:04 -0500

    Update segTree.cpp
    Updates and sum within range works

--- segTree.cpp
+++ segTree.cpp
@@ -24,12 +24,10 @@ void build_tree(vector<long long>input){
 }
 
 long long get_sum_in_range(int want_low, int want_high, int curr_node_idx = 1, int curr_low=1, int curr_high = -10){
-    // cout << curr_low << " " << curr_high << endl;
     if (curr_high < 0){
         curr_high = arr.size() / 2;
     }
     if (curr_low >= want_low && want_high >= curr_high){
-        // cout << "Returning " << arr[curr_node_idx] << endl;
         return arr[curr_node_idx];
     }
 
@@ -49,6 +47,16 @@ long long get_sum_in_range(int want_low, int want_high, int curr_node_idx = 1, i
     + get_sum_in_range(want_low, want_high, 2 * curr_node_idx + 1, right_child_curr_low,right_child_curr_high);
 }
 
+void update(int index, int value){
+    index += ((arr.size() / 2) - 1);
+    arr[index] = value;
+    index /= 2;
+    while(index != 0){
+        arr[index] = arr[2*index] + arr[2*index+1];
+        index/=2;
+    }
+}
+
 void solve(){
     int n, q;
     cin >> n >> q;
@@ -58,15 +66,22 @@ void solve(){
     }
     build_tree(input);
     for(int i = 0; i < q; i++){
-        int first, second;
-        cin >> first >> second;
-        cout << get_sum_in_range(first, second) << endl;
+        int type, first, second;
+        cin >> type >> first >> second;
+        if (type == 1){
+            update(first, second);
+        }
+        else{
+            first = min(first, second);
+            second = max(first, second);
+            cout << get_sum_in_range(first, second) << endl;
+        }        
     }
 }
 
 int main(){
     ios_base::sync_with_stdio(false);  
-    cin.tie(NULL); // flushes cout  
+    cin.tie(NULL); 
     solve();
     return 0;
 }
